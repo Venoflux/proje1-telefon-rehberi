20d494015a2c29f2397bb8013257cf113c412bb1
Venoflux <erolsarpk@gmail.com>
2021-12-07 16:36:44 +0100

    NumaraEkle function included in Choice.cs

--- Choice.cs
+++ Choice.cs
@@ -2,6 +2,30 @@ namespace proje1_telefon_rehberi
 {
     public static class Choice
     {
-        
+        static List<Person> personList = new List<Person>(){
+            new Person("Ali", "Bozkurt", "5327514141"),
+
+        };
+
+        /// Working for now however better methods needed to receive prefered inputs
+        public static void NumaraEkle()
+        {
+            Console.WriteLine("Lütfen isim giriniz             : ");
+            string ad = Console.ReadLine();
+
+            Console.WriteLine("Lütfen soyisim giriniz          : ");
+            string soyisim = Console.ReadLine();
+
+            Console.WriteLine("Lütfen telefon numarası giriniz : ");
+            string numara = Console.ReadLine();
+
+
+            Person yenikullanıcı = new Person(ad, soyisim, numara);
+            personList.Add(yenikullanıcı);
+
+            Console.WriteLine("Yeni kullanıcı başarıyla eklendi. ");
+            Console.ReadKey();
+            Console.Clear();
+        }
     }
 }
